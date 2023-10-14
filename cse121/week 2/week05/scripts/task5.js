91a0017af0044478906fb246f0768af0fb813e7f
Chris-F-Blank <131714802+Chris-F-Blank@users.noreply.github.com>
2023-10-14 17:51:17 -0600

    polishing

--- cse121/week 2/week05/scripts/task5.js
+++ cse121/week 2/week05/scripts/task5.js
@@ -3,23 +3,29 @@
 /* IF/ELSE IF */
 
 // Step 1: Declare and initialize a new variable to hold the current date
+
 const today = new Date();
 
 // Step 2: Declare another variable to hold the day of the week
+
 let dayOfWeek;
 
 // Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
+
 dayOfWeek = today.getDay();
 
 // Step 4: Declare a variable to hold a message that will be displayed
+
 let message1;
 
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
+
 if (dayOfWeek >= 1 && dayOfWeek <= 5) {
   message1 = "Hang in there!";
 }
 
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
+
 if (dayOfWeek >= 1 && dayOfWeek <= 5) {
   message1 = "Hang in there!";
 } else {
@@ -29,9 +35,11 @@ if (dayOfWeek >= 1 && dayOfWeek <= 5) {
 /* SWITCH, CASE, BREAK */
 
 // Step 1: Declare a new variable to hold another message
+
 let message2;
 
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
+
 switch (dayOfWeek) {
   case 0:
     message2 = "Sunday";
@@ -62,9 +70,12 @@ switch (dayOfWeek) {
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
+
 document.querySelector("#message1").textContent = message1;
 
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
+
+
 document.querySelector("#message2").textContent = message2;
 
 /* FETCH */
@@ -109,6 +120,7 @@ const output = (temples) => {
 // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
 // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier. Make sure the the execution of the code waits here as well until it finishes.
 // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+
 const getTemples = async () => {
   const response = await fetch(
     "https://byui-cse.github.io/cse121b-course/week05/temples.json"
@@ -127,6 +139,7 @@ getTemples();
 //     });
 
 // Step 8: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
+
 const reset = () => {
   document.querySelector("#temples").innerHTML = "";
 };
@@ -135,6 +148,7 @@ const reset = () => {
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
 // - Calls the output function passing in the sorted list of temples
+
 const sortBy = () => {
   reset();
 
@@ -180,9 +194,5 @@ const sortBy = () => {
 };
 
 // Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
+
 document.querySelector("#sortBy").addEventListener("change", sortBy);
-
-/* STRETCH */
-
-// Consider adding a "Filter by" feature that allows users to filter the list of temples
-// This will require changes to both the HTML and the JavaScript files
